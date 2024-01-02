41764c9cd5d5d748857d0e6f8dd0755e958523db
Tanveer Gupta <gupttan@amazon.co.uk>
2024-01-02 12:12:20 +0000

    add cdk constructs

---
+++ lib/rest-api-gateway-jwt-cognito-stack.ts
@@ -0,0 +1,85 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+
+import path = require("path");
+
+export class RestApiGatewayJwtCognitoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // create Cognito UserPool
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // create and add Application Integration for the User Pool
+    // and add support for oAuth / JWT tokens
+    const appIntegrationClient = userPool.addClient("WebClient", {
+      userPoolClientName: "MyAppWebClient",
+      idTokenValidity: cdk.Duration.days(1),
+      accessTokenValidity: cdk.Duration.days(1),
+      authFlows: {
+        adminUserPassword: true
+      },
+      oAuth: {
+        flows: {authorizationCodeGrant: true},
+        scopes: [cognito.OAuthScope.OPENID]
+      },
+      supportedIdentityProviders: [cognito.UserPoolClientIdentityProvider.COGNITO]
+    });
+
+    // create a Lambda function for API Gateway to invoke
+    const lambdaFn = new lambda.Function(this, "lambdaFn", {
+      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda/simple-function")),
+      runtime: lambda.Runtime.PYTHON_3_11,
+      timeout: cdk.Duration.seconds(40),
+      handler: "lambda_function.lambda_handler",
+    });
+
+    // API Gateway Definition
+    const restApi = new apigw.RestApi(this, "RestAPI", {
+      restApiName: "simpleRestAPI",
+    });
+
+    // capturing architecture for docker container (arm or x86)
+    const dockerPlatform = process.env["DOCKER_CONTAINER_PLATFORM_ARCH"]
+
+    // Authorizer function for JWT tokens
+    const dockerfile = path.join(__dirname, "../lambda/dockerized-jwt-auth-function/");
+    const code = lambda.Code.fromAssetImage(dockerfile);
+    const jwtAuthLayeredFn = new lambda.Function(this, "jwtAuthLayeredFn", {
+      code: code,
+      handler: lambda.Handler.FROM_IMAGE,
+      runtime: lambda.Runtime.FROM_IMAGE,
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 512,
+      architecture: dockerPlatform == "arm" ? lambda.Architecture.ARM_64 : lambda.Architecture.X86_64,
+      environment: {
+          "API_ID": restApi.restApiId,
+          "API_REGION": this.region,
+          "ACCOUNT_ID": this.account,
+          "COGNITO_USER_POOL_ID": userPool.userPoolId,
+          "COGNITO_APP_CLIENT_ID": appIntegrationClient.userPoolClientId    
+      }
+    });
+
+    // create JWT token authorizer for the API Gateway endpoint
+    const tokenAuthorizer = new apigw.TokenAuthorizer(this, 'jwttokenAuth', {
+      handler: jwtAuthLayeredFn,
+      validationRegex: "^(Bearer )[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)$"
+      });
+
+    // create a GET /hello endpoint which invoked the simple lambda function,
+    // and uses the token authorizer created above
+    const summarizeEndpoint = restApi.root.addResource('hello');
+    summarizeEndpoint.addMethod(
+      "GET", 
+      new apigw.LambdaIntegration(lambdaFn),
+      { authorizer: tokenAuthorizer }
+    );
+
+  }
+}
---
+++ test/rest-api-gateway-jwt-cognito.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RestApiGatewayJwtCognito from '../lib/rest-api-gateway-jwt-cognito-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rest-api-gateway-jwt-cognito-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RestApiGatewayJwtCognito.RestApiGatewayJwtCognitoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
